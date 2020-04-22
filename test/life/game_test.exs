59114b3fceab3199971d5b9b0612b4b7fc308d2c
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-04-22 10:52:42 -0700

    fixed tests

--- test/life/game_test.exs
+++ test/life/game_test.exs
@@ -3,18 +3,18 @@ defmodule Life.GameTest do
   use GameBuilders
 
   test "Valid board positions dont wrap around" do
-    game = Game.new(2)
+    game = Game.new(4)
     assert Game.wrap_cell(game, {1, 1}) == {1, 1}
-    assert Game.wrap_cell(game, {1, 2}) == {1, 2}
-    assert Game.wrap_cell(game, {2, 1}) == {2, 1}
-    assert Game.wrap_cell(game, {2, 2}) == {2, 2}
+    assert Game.wrap_cell(game, {1, 4}) == {1, 4}
+    assert Game.wrap_cell(game, {4, 1}) == {4, 1}
+    assert Game.wrap_cell(game, {4, 4}) == {4, 4}
   end
 
   test "Board position indexes wrap around" do
-    game = Game.new(2)
-    assert Game.wrap_cell(game, {0, 0}) == {2, 2}
-    assert Game.wrap_cell(game, {1, 3}) == {1, 1}
-    assert Game.wrap_cell(game, {3, 2}) == {1, 2}
-    assert Game.wrap_cell(game, {3, 3}) == {1, 1}
+    game = Game.new(4)
+    assert Game.wrap_cell(game, {0, 0}) == {4, 4}
+    assert Game.wrap_cell(game, {1, 5}) == {1, 1}
+    assert Game.wrap_cell(game, {5, 2}) == {1, 2}
+    assert Game.wrap_cell(game, {5, 5}) == {1, 1}
   end
 end
