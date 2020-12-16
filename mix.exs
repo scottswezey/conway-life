ea47088a36085a9f78f79efe27517cd656bdc73a
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-12-15 21:59:32 -0800

    Enable OS data in dashboard

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Life.MixProject do
   def application do
     [
       mod: {Life.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
